a168895474f153bef8eb067f0c863c63c3bef455
Krzysztof Gacek <gkrzysztof92@gmail.com>
2015-06-09 11:38:43 +0200

    Created class PrimeNumber, added isPrime method

---
+++ src/PrimeNumber.java
@@ -0,0 +1,17 @@
+
+public class PrimeNumber {
+	
+	public static boolean isPrime(long number) {
+		
+		if (number < 2) {
+			return false;
+		}
+		for (int i = 2; i * i <= number; i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
