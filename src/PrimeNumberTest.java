709123ca039fdeb3a1121163ff3fe3262608dd2f
Krzysztof Gacek <gkrzysztof92@gmail.com>
2015-06-09 18:18:42 +0200

    In JUnit Test Case PrimeNumberTest, created test for getPrimeNumber method

--- src/PrimeNumberTest.java
+++ src/PrimeNumberTest.java
@@ -15,5 +15,12 @@ public class PrimeNumberTest {
 	public void findPrimeNumber() {
 		assertEquals(11, PrimeNumber.findPrimeNumber(5));
 	}
+	
+	@Test
+	public void getPrimeNumber() {
+		PrimeNumber test = new PrimeNumber(10);
+		assertEquals(29, test.getPrimeNumber());
+	}
+	
 
 }
